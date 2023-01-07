0625e996a1f93371ef9563704fa95389181cb3b5
jeronimo c l ramos cruz de lima ramos <dacruz1990@hotmail.com>
2023-01-07 20:40:14 -0300

    adicionar metodo para calcular bhaskara.

---
+++ Controllers/BhaskaraController.cs
@@ -0,0 +1,21 @@
+﻿using BhascaraModel;
+using BhaskaraService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bhaskara1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BhaskaraController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ObterResultado")]
+        public RetornoXX ObterResultado(double a, double b, double c)
+        {
+            var x = new Calculo().CalculoBhaskara(a, b, c);
+            return x;
+
+        }
+    }
+}
